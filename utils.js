db667f39c6829d07249e799b3674fd74970220c2
Rui Machado <45498752+ruipedromachado@users.noreply.github.com>
2022-10-07 09:45:16 +0100

    files

--- utils.js
+++ utils.js
@@ -3,14 +3,19 @@ const fs = require('fs');
 require('dotenv').config();
 
 module.exports = {
-    getDateRange: () => {  
+    getDateRange: (justYesterday) => {  
         var payload = {};
-        var date = new Date();
-        date.setDate(date.getDate()-1);
-        date.setHours(0,0,0,0);
-        payload.startDate = date.toJSON();
-        date.setHours(23,59,59,0);
-        payload.endDate = date.toJSON();
+        var yesterday = new Date();
+        yesterday.setDate(yesterday.getDate()-1);
+        yesterday.setHours(0,0,0,0);
+        payload.startDate = yesterday.toJSON();
+        if (justYesterday) {
+            yesterday.setHours(23,59,59,0);
+            payload.endDate = yesterday.toJSON();
+        } else {
+            var today = new Date();
+            payload.endDate = today.toJSON();
+        }
         return payload;
     },
     getRandom: () => { return new Date().getTime(); },
@@ -18,7 +23,14 @@ module.exports = {
         var payload = ["time,connectorName,actionName,totalExistingErrors,totalErrorsExported,errorDescription"];
         data.forEach(entry => {
             entry.errorDetails.forEach(detail => {
-                payload.push([detail.time, entry.connectorName, entry.actionName, entry.totalErrors, entry.errorDetails.length, detail.message.replace(/\n/ig, "").replace(/,/ig, "|")].join(","));
+                payload.push([
+                    detail.time, 
+                    '"' + entry.connectorName.replace(/"/ig, "").replace(/\n/ig, "").replace(/,/ig, "|") + '"', 
+                    '"' + entry.actionName.replace(/"/ig, "").replace(/\n/ig, "").replace(/,/ig, "|") + '"', 
+                    entry.totalErrors, 
+                    entry.errorDetails.length, 
+                    '"' + detail.message.replace(/"/ig, "").replace(/\n/ig, "").replace(/,/ig, "|") + '"'
+                ].join(","));
             });
         });
         fs.writeFileSync('./output/' + process.env.CDH_ACCOUNT + "-" + process.env.CDH_PROFILE + "-" + new Date().getTime() + ".CSV", payload.join('\n'));
